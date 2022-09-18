1d117d1a97e50a4637ead911f152a4e4863781a9
gihu22 <113820601+gihu22@users.noreply.github.com>
2022-09-18 16:01:36 +0700

    Update vers2.sh

--- vers2.sh
+++ vers2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 apt update && apt upgrade
-apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
 apt-get install git
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.7/SRBMiner-Multi-1-0-7-Linux.tar.xz
 tar -xf  SRBMiner-Multi-1.0.7.tar.xz
