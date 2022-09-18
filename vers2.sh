e025254e890ca2592b81a99c4ef28a6a36bc86aa
gihu22 <113820601+gihu22@users.noreply.github.com>
2022-09-18 16:03:10 +0700

    Update vers2.sh

--- vers2.sh
+++ vers2.sh
@@ -2,6 +2,6 @@
 apt update && apt upgrade
 apt-get install git
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.7/SRBMiner-Multi-1-0-7-Linux.tar.xz
-tar -xf  SRBMiner-Multi-1.0.7.tar.xz
+tar -xfv SRBMiner-Multi-1.0.7.tar.xz
 cd SRBMiner-Multi-1.0.7
 ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool stratum+tcp://ap.luckpool.net:3956#xnsub --wallet RNiRxFznYcRHNpcKTut5ymNV1CL1WHqnNv
